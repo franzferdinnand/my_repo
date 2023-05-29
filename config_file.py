6361cc37dac2b97a1a105fed25ef5cf5ab6c2838
franzferdinnand <110455433+franzferdinnand@users.noreply.github.com>
2023-05-29 14:29:15 +0200

    Create config_file.py

---
+++ config_file.py
@@ -0,0 +1,28 @@
+URL = 'https://script.google.com/macros/s/AKfycby3Fp-' \
+      'J3N0OM5UZhg9SHgIusHBMC2kWVXSOVsP26smPTaYS_4IiOT7sVx7ZWyC3XsVW7g/exec'
+
+URL_FOR_WEATHER = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid=47503' \
+                  'e85fabbabc93cff28c52398ae97&units=metric'
+
+SMTP_SERVER = 'smtp.ukr.net'
+PASSWORD_API = 'Q8HtF2xtWn80FPxb'
+USER_ = 'test_hillel_api_mailing@ukr.net'
+password_for_email = 'qwertyuiop123456789'
+
+message_to_send = 'Hi, {}! Today, {}, in the {} city the temperature is {}C, {} {}, humidiy is {}%'
+
+weather_smiles = {
+      '11d': '\U000026C8',
+      '09d': '\U0001F327',
+      '10d': '\U0001F327',
+      '13d': '\U00002746',
+      '03d': '\U000026C5',
+      '02d': '\U0001F324',
+      '02n': '\U0001F324',
+      '03n': '\U000026C5',
+      '04d': '\U0001F325',
+      '04n': '\U0001F325',
+      '50d': '\U0001F32B',
+      '01d': '\U0001F31E',
+      '01n': '\U0001F31E',
+}
